161663422e8e719a939381f73f266a9dc3164484
DawidPodpora <138831149+DawidPodpora@users.noreply.github.com>
2025-01-20 20:11:04 +0100

    Dodanie tłumaczenia do wiadomości prywatnych

--- client/src/components/ContentComponents/PrivateMessages.js
+++ client/src/components/ContentComponents/PrivateMessages.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 function PrivateMessages({data}) {
     const [messages, setMessages] = useState([]); //Przechowywanie wiadomości użytkownika
     const [newMessage, setNewMessage] = useState({receiver: '', title: '', content: ''});//Nowa wiadomość
@@ -9,6 +10,7 @@ function PrivateMessages({data}) {
     const [showReplyModal, setShowReplyModal] = useState(false);
     const token = localStorage.getItem('token');
     const currentUser = data.username;
+    const { t } = useTranslation();
 
     const fetchMessages = async () => {
         try{
@@ -20,13 +22,12 @@ function PrivateMessages({data}) {
             });
             if (response.ok){
                 const data = await response.json();
-                console.log('Pobrane wiadomości: ')
                 setMessages(data);//Ustawienie wiadomości
             }else{
-                console.error('Błąd podczas pobierania wiadomości');
+                console.error(t('fetchMessagesError'));
             }
         } catch(error){
-            console.error('Błąd serwera:',error);
+            console.error(t('serverError'),error);
         }
     };
     //Pobieranie wiadomości po załadowaniu komponentu
@@ -36,7 +37,7 @@ function PrivateMessages({data}) {
     //Wysyłanie wiadomości do wszystkich
     const sendMessageToAll = async() => {
         if(!newMessageToAll.title || !newMessageToAll.content){
-            alert('Tytuł i treść są wymagane!');
+            alert(t('titleAndContent'));
             return;
         }
         try{
@@ -57,7 +58,7 @@ function PrivateMessages({data}) {
                 alert(`Błąd: ${errorData.error || 'Nie udało się wysłać wiadomości do wszystkich użytkowników.'}`);
             }
         }catch(error){
-            console.error('Błąd serwera: ',error);
+            console.error(t('serverError'),error);
         }
     };
     //Wysyłąnie nowej wiadomości
@@ -89,7 +90,7 @@ function PrivateMessages({data}) {
                 alert(`Błąd: ${errorData.error || 'Nie udało się wysłać wiadomości.'}`);
             }
         } catch (error) {
-            console.error('Błąd serwera:', error);
+            console.error(t('serverError'), error);
         }
     };
     //Usuwanie wiadomości
@@ -104,10 +105,10 @@ function PrivateMessages({data}) {
             if (response.ok) {
                 setMessages((prevMessages) => prevMessages.filter((msg) => msg._id !== messageId));
             } else {
-                console.error('Błąd podczas usuwania wiadomości');
+                console.error(t('deleteMessagesError'));
             }
         } catch (error){
-            console.error('Błąd serwera:', error);
+            console.error(t('serverError'), error);
         }
     };
     //Oznaczanie wiadomości jako przeczytana
@@ -142,25 +143,25 @@ function PrivateMessages({data}) {
             {/* Zielony komponent na czarnym tle */}
             <div className="w-full h-full bg-maincolor1 p-5 rounded-xl flex flex-col">
                 <div className="flex justify-between items-center mb-4">
-                    <h1 className="text-2xl font-bold">Wiadomości prywatne</h1>
+                    <h1 className="text-2xl font-bold">{t('privateMessages')}</h1>
                     <button
                     onClick={() => fetchMessages()}
                     className="bg-maincolor1 text-maincolor4 border-maincolor2 border px-4 py-2 rounded shadow hover:bg-opacity-80"
                     >
-                        Odśwież
+                        {t('refresh')}
                     </button>
                     <button
                         onClick={() => setShowSendModal(true)}
                         className="bg-maincolor1 text-maincolor4 border-maincolor2 border px-4 py-2 rounded shadow hover:bg-opacity-80"
                     >
-                        Wyślij wiadomość
+                        {t('sendMessage')}
                     </button>
                     {data.isAdmin && (
                         <button
                             onClick={() => setShowSendToAllModal(true)}
                             className="bg-maincolor4 text-black border-maincolor2 border px-4 py-2 rounded shadow hover:bg-opacity-80"
                         >
-                            Wiadomość do wszystkich
+                            {t('sendMessageToAll')}
                         </button>
                     )}
                 </div>
@@ -179,14 +180,14 @@ function PrivateMessages({data}) {
                                 <div className="flex justify-between items-center">
                                     <div>
                                         <p className="font-bold">{msg.title}</p>
-                                        <p className="text-sm">Od: {msg.senderId ? msg.senderId.username : "Nieznany użytkownik"}</p>
+                                        <p className="text-sm">{t('from')} {msg.senderId ? msg.senderId.username : t('unknownUser')}</p>
                                         <p className="text-xs">{new Date(msg.createdAt).toLocaleString()}</p>
                                     </div>
                                     <button
                                         onClick={() => deleteMessage(msg._id)}
                                         className="text-red-500 hover:text-red-700 text-sm"
                                     >
-                                        Usuń
+                                        {t('delete')}
                                     </button>
                                 </div>
                                 <div className="space-x-4 mt-2">
@@ -197,13 +198,13 @@ function PrivateMessages({data}) {
                                         }}
                                         className="mt-2 text-blue-500 hover:underline"
                                     >
-                                        {selectedMessage === msg ? "Zwiń" : "Zobacz szczegóły"}
+                                        {selectedMessage === msg ? t('hide') : t('show')}
                                     </button>
                                     <button
                                         onClick={() => openReplyModal(msg.senderId.username)}
                                         className="text-green-500 hover:underline"
                                     >
-                                        Odpowiedz
+                                        {t('reply')}
                                     </button>
                                 </div>
                                 {selectedMessage === msg && (
@@ -215,7 +216,7 @@ function PrivateMessages({data}) {
                         ) : null // Dodaj odpowiednie zachowanie, gdy senderId jest null
                     ))
                 ) : (
-                    <p className="text-center">Brak wiadomości</p>
+                    <p className="text-center">{t('noMessages')}</p>
                 )}
                 </div>
             </div>
@@ -224,16 +225,16 @@ function PrivateMessages({data}) {
             {showReplyModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center">
                     <div className="bg-maincolor1 p-6 rounded-lg shadow-md w-1/2">
-                        <h2 className="text-lg font-bold mb-4"> Odpowiedź do {newMessage.receiver}</h2>
+                        <h2 className="text-lg font-bold mb-4"> {t('replyTo')} {newMessage.receiver}</h2>
                         <input
                             type="text"
-                            placeholder="Tytuł"
+                            placeholder={t('title')}
                             value={newMessage.title}
                             onChange={(e) => setNewMessage({...newMessage, title: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
                             />
                             <textarea
-                            placeholder="Treść"
+                            placeholder={t('content')}
                             value={newMessage.content}
                             onChange={(e) => setNewMessage({...newMessage, content: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
@@ -243,13 +244,13 @@ function PrivateMessages({data}) {
                                 onClick={() => setShowReplyModal(false)}
                                 className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-600"
                             >
-                                Anuluj
+                                {t('cancel')}
                             </button>
                             <button
                                 onClick={sendMessage}
                                 className="bg-maincolor4 text-black px-4 py-2 rounded shadow hover:bg-opacity-80"
                             >
-                                Wyślij
+                                {t('send')}
                             </button>
                         </div>
                     </div>
@@ -260,16 +261,16 @@ function PrivateMessages({data}) {
             {showSendToAllModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center">
                     <div className="bg-maincolor1 p-6 rounded-lg shadow-md w-1/2">
-                        <h2 className="text-lg font-bold mb-4">Wyślij wiadomość do wszystkich</h2>
+                        <h2 className="text-lg font-bold mb-4">{t('sendMessageToAll')}</h2>
                         <input
                             type="text"
-                            placeholder="Tytuł"
+                            placeholder={t('title')}
                             value={newMessageToAll.title}
                             onChange={(e) => setNewMessageToAll({...newMessageToAll, title: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
                         />
                         <textarea
-                            placeholder="Treść"
+                            placeholder={t('content')}
                             value={newMessageToAll.content}
                             onChange={(e) => setNewMessageToAll({...newMessageToAll, content: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
@@ -282,13 +283,13 @@ function PrivateMessages({data}) {
                                 }}
                                 className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-600"
                             >
-                                Anuluj
+                                {t('cancel')}
                             </button>
                             <button
                                 onClick={sendMessageToAll}
                                 className="bg-maincolor4 text-black px-4 py-2 rounded shadow hover:bg-opacity-80"
                             >
-                                Wyślij
+                                {t('send')}
                             </button>
                         </div>
                     </div>
@@ -299,24 +300,24 @@ function PrivateMessages({data}) {
             {showSendModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center">
                     <div className="bg-maincolor1 p-6 rounded-lg shadow-md w-1/2">
-                        <h2 className="text-lg font-bold mb-4">Wyślij wiadomość</h2>
+                        <h2 className="text-lg font-bold mb-4">{t('sendMessage')}</h2>
                         <input
                             type="text"
-                            placeholder="Odbiorca"
+                            placeholder={t('receiver')}
                             value={newMessage.receiver}
                             onChange={(e) => setNewMessage({...newMessage, receiver: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
                         />
                         <input
                             type="text"
-                            placeholder="Tytuł"
+                            placeholder={t('title')}
                             value={newMessage.title}
                             onChange={(e) => setNewMessage({...newMessage, title: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
                         />
                         <textarea
                             type="text"
-                            placeholder="Treść"
+                            placeholder={t('content')}
                             value={newMessage.content}
                             onChange={(e) => setNewMessage({...newMessage, content: e.target.value})}
                             className="w-full mb-4 p-2 border rounded bg-black text-maincolor4"
@@ -329,13 +330,13 @@ function PrivateMessages({data}) {
                                 }}
                                 className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-600"
                             >
-                                Anuluj
+                                {t('cancel')}
                             </button>
                             <button
                                 onClick={sendMessage}
                                 className="bg-maincolor4 text-black px-4 py-2 rounded shadow hover:bg-opacity-80"
                             >
-                                Wyślij
+                                {t('send')}
                             </button>
                         </div>
                     </div>
